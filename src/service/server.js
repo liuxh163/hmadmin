a818a9181aa5fa7358f30c6756d7043a9f253678
古韵 <grainrain@hotmail.com>
2019-01-26 20:17:49 +0800

    调整域名

--- src/service/server.js
+++ src/service/server.js
@@ -7,11 +7,11 @@ let _port = ''
 export const release = true // 发布 true | false
 
 if (release === false) {
-    _ip = '10.1.99.7'
-    _port = '4000'
-} else if (release === true) {
     _ip = '47.92.131.110'
     _port = '24651'
+} else if (release === true) {
+    _ip = 'http://hmapi.haima101.com'
+    _port = '80'
 }
 
 export const port = _port
